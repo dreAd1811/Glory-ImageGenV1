61b79459df4aa275b824ce84584527974c1c298b
dread <dreadcreator5@gmail.com>
2023-09-08 15:35:41 +0530

    add 3d background using three.js
    -drop gradient shadow of output image
    
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- public/javascripts/background.js
+++ public/javascripts/background.js
@@ -1,62 +1,167 @@
-let scene, camera, renderer, stars, starGeo;
+// let scene, camera, renderer, stars, starGeo;
 
-    function init() {
+// function init() {
 
-      scene = new THREE.Scene();
+//   scene = new THREE.Scene();
 
-      camera = new THREE.PerspectiveCamera(60,window.innerWidth / window.innerHeight, 1, 1000);
-      camera.position.z = 1;
-      camera.rotation.x = Math.PI/2;
+//   camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+//   camera.position.z = 1;
+//   camera.rotation.x = Math.PI / 2;
 
-      renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight);
-      document.body.appendChild(renderer.domElement);
+//   renderer = new THREE.WebGLRenderer();
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+//   document.body.appendChild(renderer.domElement);
 
-      starGeo = new THREE.Geometry();
-      for(let i=0;i<6000;i++) {
-        star = new THREE.Vector3(
-          Math.random() * 600 - 300,
-          Math.random() * 600 - 300,
-          Math.random() * 600 - 300
-        );
-        star.velocity = 0;
-        star.acceleration = 0.02;
-        starGeo.vertices.push(star);
-      }
+//   starGeo = new THREE.Geometry();
+//   for (let i = 0; i < 6000; i++) {
+//     star = new THREE.Vector3(
+//       Math.random() * 600 - 300,
+//       Math.random() * 600 - 300,
+//       Math.random() * 600 - 300
+//     );
+//     star.velocity = 0;
+//     star.acceleration = 0.02;
+//     starGeo.vertices.push(star);
+//   }
 
-      let sprite = new THREE.TextureLoader().load( 'star.png' );
-      let starMaterial = new THREE.PointsMaterial({
-        color: 0xaaaaaa,
-        size: 0.7,
-        map: sprite
-      });
+//   let sprite = new THREE.TextureLoader().load('star.png');
+//   let starMaterial = new THREE.PointsMaterial({
+//     color: 0xaaaaaa,
+//     size: 0.7,
+//     map: sprite
+//   });
 
-      stars = new THREE.Points(starGeo,starMaterial);
-      scene.add(stars);
+//   stars = new THREE.Points(starGeo, starMaterial);
+//   scene.add(stars);
 
-      window.addEventListener("resize", onWindowResize, false);
+//   window.addEventListener("resize", onWindowResize, false);
 
-      animate(); 
-    }
-    function onWindowResize() {
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-      }
-    function animate() {
-      starGeo.vertices.forEach(p => {
-        p.velocity += p.acceleration
-        p.y -= p.velocity;
-        
-        if (p.y < -200) {
-          p.y = 200;
-          p.velocity = 0;
-        }
-      });
-      starGeo.verticesNeedUpdate = true;
-      stars.rotation.y +=0.002;
-    
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
-    }
-    init();
+//   animate();
+// }
+// function onWindowResize() {
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+// }
+// function animate() {
+//   starGeo.vertices.forEach(p => {
+//     p.velocity += p.acceleration
+//     p.y -= p.velocity;
+
+//     if (p.y < -200) {
+//       p.y = 200;
+//       p.velocity = 0;
+//     }
+//   });
+//   starGeo.verticesNeedUpdate = true;
+//   stars.rotation.y += 0.002;
+
+//   renderer.render(scene, camera);
+//   requestAnimationFrame(animate);
+// }
+// init();
+
+var renderer, scene, camera, composer, circle, skelet, particle;
+
+window.onload = function() {
+  init();
+  animate();
+}
+
+function init() {
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.autoClear = false;
+  renderer.setClearColor(0x000000, 0.0);
+  document.getElementById('threejs-container').appendChild(renderer.domElement);
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.z = 400;
+  scene.add(camera);
+
+  circle = new THREE.Object3D();
+  skelet = new THREE.Object3D();
+  particle = new THREE.Object3D();
+
+  scene.add(circle);
+  scene.add(skelet);
+  scene.add(particle);
+
+  var geometry = new THREE.TetrahedronGeometry(2, 0);
+  var geom = new THREE.IcosahedronGeometry(7, 1);
+  var geom2 = new THREE.IcosahedronGeometry(15, 1);
+
+  var material = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    shading: THREE.FlatShading
+  });
+
+  for (var i = 0; i < 1000; i++) {
+    var mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
+    mesh.position.multiplyScalar(90 + (Math.random() * 700));
+    mesh.rotation.set(Math.random() * 2, Math.random() * 2, Math.random() * 2);
+    particle.add(mesh);
+  }
+
+  var mat = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    shading: THREE.FlatShading
+  });
+
+  var mat2 = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    wireframe: true,
+    side: THREE.DoubleSide
+
+  });
+
+  var planet = new THREE.Mesh(geom, mat);
+  planet.scale.x = planet.scale.y = planet.scale.z = 16;
+  circle.add(planet);
+
+  var planet2 = new THREE.Mesh(geom2, mat2);
+  planet2.scale.x = planet2.scale.y = planet2.scale.z = 10;
+  skelet.add(planet2);
+
+  var ambientLight = new THREE.AmbientLight(0x999999 );
+  scene.add(ambientLight);
+  
+  var lights = [];
+lights[0] = new THREE.DirectionalLight( 0xffffff, 1 );
+lights[0].position.set( 1, 0, 0 );
+lights[1] = new THREE.DirectionalLight( 0x11E8BB, 1 );
+lights[1].position.set( 0.75, 1, 0.5 );
+lights[2] = new THREE.DirectionalLight( 0x8200C9, 1 );
+lights[2].position.set( -0.75, -1, 0.5 );
+scene.add( lights[0] );
+scene.add( lights[1] );
+scene.add( lights[2] );
+  
+
+  window.addEventListener('resize', onWindowResize, false);
+
+};
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  particle.rotation.x += 0.0000;
+  particle.rotation.y -= 0.0040;
+  circle.rotation.x -= 0.0020;
+  circle.rotation.y -= 0.0030;
+  skelet.rotation.x -= 0.0010;
+  skelet.rotation.y += 0.0020;
+  renderer.clear();
+
+  renderer.render( scene, camera )
+};
